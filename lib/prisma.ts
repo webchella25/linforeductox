abc46fda5b39553368cb89db7e588de2e9426f07
webchella25 <webchella@gmail.com>
2025-11-07 17:57:46 +0100

    fix: Agregar named export para prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,17 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const client = new PrismaClient().$extends(withAccelerate());
+
+export const prisma = globalForPrisma.prisma ?? client;
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma as any;
+}
 
 export default prisma;
