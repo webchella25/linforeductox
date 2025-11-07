dd410e8b50f745eb72434459384780b68cb5f1f8
webchella25 <webchella@gmail.com>
2025-11-07 17:31:15 +0100

    Configuro Prisma para Accelerate con dataProxy

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,6 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
